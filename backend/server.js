e3255b8683c898fc099dbc88ff51ac192e814426
larissamartinsz <larissa.souza@sou.inteli.edu.br>
2025-08-31 20:27:02 -0300

    fix: ajusta server

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,18 @@
+const { Sequelize } = require('sequelize');
 require('dotenv').config();
-const express = require('express');
-const { sequelize } = require('./db');
-const contactsRouter = require('./routes/contacts');
-const Contact = require('./models/contact');
 
-const app = express();
-app.use(express.json());
-app.use('/contacts', contactsRouter);
-
-const PORT = process.env.PORT || 3000;
-
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Conectado ao DB RDS');
-    await sequelize.sync(); // cria a tabela se não existir
-    app.listen(PORT, () => console.log(`API rodando na porta ${PORT}`));
-  } catch (err) {
-    console.error('Erro ao conectar DB', err);
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+  port: process.env.DB_PORT,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
-})();
+});
+
+sequelize.authenticate()
+  .then(() => console.log('Conectado ao RDS!'))
+  .catch(err => console.error('Erro ao conectar DB', err));
